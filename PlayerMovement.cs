c940316f8c255da63c4ede0068d5cc36769967b2
MUCHUAN SU <87951485+MuChuanSu@users.noreply.github.com>
2022-02-24 09:43:49 +0000

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     Animator myAnimator;
 
 
-
+    //declare references in Start()
     void Start()
     {
         myRigid = GetComponent<Rigidbody2D>();
@@ -34,14 +34,22 @@ public class PlayerMovement : MonoBehaviour
     }
     void Run()
     {
+        //create a new Vector2 and multiplies x to speed up the horizontal moving speed of the regidbody,
+        //assign the vector2 as the regidbody velocity.
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed,myRigid.velocity.y);
         myRigid.velocity = playerVelocity;
-
+        //epislon is the smallest float the isn't 0 
+        //I'll just pretend it is 0
+        //the code below means [if the horizontal speed is not zero] which means [if it is moving] then play the isRunning animation.
         bool playerHorizontalSpeed = Mathf.Abs(myRigid.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("isRunning",playerHorizontalSpeed);
     }
-    void OnJump(InputValue value)
+    
+    
+    void OnJump(InputValue value)  //OnJUmp is automatically recognized by unity, which will run when the jump button was pressed
     {
+    //if it is pressed and also the capsulecollider of the player is touching Groundlayer(is on ground)
+    //then add velocity to y so it can jump ONCE 
         if (value.isPressed && myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             myRigid.velocity += new Vector2(0f,jumpSpeed);
@@ -50,8 +58,12 @@ public class PlayerMovement : MonoBehaviour
 
     void FlipSprite()
     {
+        // if the player is moving, change the way it faces based on the velocity(if velocity is positive,face right, vice versa
+        //Mathf.Sign() Returns the sign of f.
+        //Return value is 1 when f is positive or zero, -1 when f is negative.
+        
+        
         bool playerHorizontalSpeed = Mathf.Abs(myRigid.velocity.x)>Mathf.Epsilon;
-
         if (playerHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigid.velocity.x), 1f);
